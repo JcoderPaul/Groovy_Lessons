d8c9070839ee423006619abc50ae1568266aec23
Paul <100008373+JcoderPaul@users.noreply.github.com>
2023-07-07 13:04:20 +0300

    Add Teacher.java in Groovy_part_2

---
+++ Groovy_part_2/src/main/groovy/com/oldboy/lesson_7/Teacher.java
@@ -0,0 +1,13 @@
+package com.oldboy.lesson_7;
+
+public class Teacher {
+    private final Integer id;
+
+    public Teacher(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+}
