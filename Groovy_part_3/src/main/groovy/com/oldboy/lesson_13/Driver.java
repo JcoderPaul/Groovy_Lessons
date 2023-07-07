e123cb0a5c721931db906f862bbd58405485575e
Paul <100008373+JcoderPaul@users.noreply.github.com>
2023-07-07 13:24:05 +0300

    Add Driver.java in Groovy_part_3

---
+++ Groovy_part_3/src/main/groovy/com/oldboy/lesson_13/Driver.java
@@ -0,0 +1,13 @@
+package com.oldboy.lesson_13;
+
+public class Driver {
+    private final int id;
+
+    public Driver(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
